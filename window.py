a7882cfe3ef03aa176dd54cf1c08d00951209fad
Miguel Gueifao <miguel.gueifao@gmail.com>
2025-03-15 14:28:07 +0100

    Changed camera movement calculation to allow for movements in maps smaller then window size

--- window.py
+++ window.py
@@ -28,6 +28,7 @@ class window:
         self.scroll_speed = 10
         self.camera_x = (self.tilemap_width - self.window_width) // 2
         self.camera_y = (self.tilemap_height - self.window_height) // 2
+        self._calculate_camera_borders()
 
     def _create_magnifying_glass_icon(self) -> pygame.Surface:
         size = 32
@@ -143,6 +144,20 @@ class window:
             max(self.zoom_level // 2, 5),
         )
 
+    def _calculate_camera_borders(self) -> None:
+        if 0 < self.tilemap_width - self.window_width:
+            self.min_camera_x = 0
+            self.max_camera_x = self.tilemap_width - self.window_width
+        else:
+            self.min_camera_x = self.tilemap_width - self.window_width
+            self.max_camera_x = 0
+        if 0 < self.tilemap_height - self.window_height:
+            self.min_camera_y = self.tilemap_height - self.window_height
+            self.max_camera_y = 0
+        else:
+            self.min_camera_y = 0
+            self.max_camera_y = self.tilemap_height - self.window_height
+
     def update_map_dimensions(self, change: int) -> None:
         old_zoom = self.zoom_level
         self.zoom_level = max(1, min(4, self.zoom_level + change))  # Increased zoom factor
@@ -161,8 +176,7 @@ class window:
         new_world_y = rel_y * self.tilemap_height
         self.camera_x = int(new_world_x - mouse_x)
         self.camera_y = int(new_world_y - mouse_y)
-        self.camera_x = max(0, min(self.camera_x, self.tilemap_width - self.window_width))
-        self.camera_y = max(0, min(self.camera_y, self.tilemap_height - self.window_height))
+        self._calculate_camera_borders()
 
     def render(self):
         running = True
@@ -172,6 +186,12 @@ class window:
 
         while running:
             pygame.event.pump()
+
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_ESCAPE] or keys[pygame.K_q]:
+                running = False
+                continue
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
@@ -185,17 +205,17 @@ class window:
                         self.update_map_dimensions(-1)
                         print(f"Debug: New zoom level: {self.zoom_level}")
 
-            keys = pygame.key.get_pressed()
+            scroll_x = scroll_y = 0
             if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-                self.camera_x = max(self.camera_x - self.scroll_speed, 0)
+                scroll_x -= self.scroll_speed
             if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-                self.camera_x = min(self.camera_x + self.scroll_speed, self.tilemap_width - self.window_width)
+                scroll_x += self.scroll_speed
             if keys[pygame.K_UP] or keys[pygame.K_w]:
-                self.camera_y = max(self.camera_y - self.scroll_speed, 0)
+                scroll_y += self.scroll_speed
             if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-                self.camera_y = min(self.camera_y + self.scroll_speed, self.tilemap_height - self.window_height)
-            if keys[pygame.K_ESCAPE]:
-                running = False
+                scroll_y -= self.scroll_speed
+            self.camera_x = max(min(self.camera_x + scroll_x, self.max_camera_x), self.min_camera_x)
+            self.camera_y = min(max(self.camera_y - scroll_y, self.max_camera_y), self.min_camera_y)
 
             self.screen.fill((0, 0, 0))
             self.screen.blit(
