6c6001521bc6445aa5c4f6228ea572adc2b0bb70
Miguel Gueifao <miguel.gueifao@gmail.com>
2025-03-17 21:45:58 +0100

    Made both ctrls switch tools and and added event handler

--- tools/tool_belt.py
+++ tools/tool_belt.py
@@ -1,5 +1,6 @@
 import pygame
 from loguru import logger
+from pygame.event import Event
 
 from tools import Eraser, Pencil, Tool
 
@@ -19,11 +20,20 @@ class ToolBelt:
         self.current.process_keys(window)
 
     def process_keydown(self, window, key):
-        if pygame.key.get_pressed()[pygame.K_LCTRL] and key in [*self.all_tools]:
+        if (pygame.key.get_pressed()[pygame.K_LCTRL] or pygame.key.get_pressed()[pygame.K_RCTRL]) and key in [
+            *self.all_tools
+        ]:
             self.current = self.all_tools[key]
             logger.debug(f"Set tool to {str(self.current)}")
             return
         self.current.process_keydown(window, key)
+
+    def handle_event(self, window, event: Event):
+        match event.type:
+            case pygame.MOUSEBUTTONDOWN:
+                self.current.process_mouse_down(window, event)
+            case _:
+                pass
 
     def render(self, window):
         self.current.render(window)
