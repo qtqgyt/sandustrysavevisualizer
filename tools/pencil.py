e576d8cf9534ac4fabaf286e3609aa70a22107df
Miguel Gueifao <miguel.gueifao@gmail.com>
2025-03-18 23:18:03 +0100

    Cleaned file

--- tools/pencil.py
+++ tools/pencil.py
@@ -5,15 +5,5 @@ class Pencil(Tool):
     def __init__(self) -> None:
         super().__init__()
 
-    def process_keys(self, window) -> None:
-        super().process_keys(window)
-
-    def process_keydown(self, window, key) -> None: ...
-    def render(self, window) -> None:
-        super().render(window)
-
-    def process_mouse(self, window) -> None: ...
-    def process_mouse_down(self, window, event) -> tuple[bool, dict | None]: ...
-
     def __str__(self) -> str:
         return "Pencil"
